80f5a5b6ccc895b383a740f2a2b9419f2d9aa969
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-09 11:33:58 +0530

    Plazone route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages";
+import { Home, Playzone } from "./pages";
 import { Navbar } from "./components";
 import React from "react";
 
@@ -10,6 +10,7 @@ function App() {
             <div className="p-4 mt-16 md:ml-20">
                 <Routes>
                     <Route path="/" element={<Home/>}/>
+                    <Route path="/playzone/:quizID" element={<Playzone/>}/>
                 </Routes>
             </div>
         </div>
