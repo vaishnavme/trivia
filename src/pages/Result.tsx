dea138b4c4cddbaa7168b9e45685876a08d01c72
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-10 19:43:45 +0530

    basic result page

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,5 +1,36 @@
+import { useData } from "../context";
+
 export default function Result() {
+    const {state: {currentQuiz, score}} = useData();
     return (
-        <h1>Result</h1>
+        <div className="m-auto w-full max-w-2xl">
+            <h1 className="text-3xl font-medium">Your Score</h1>
+            <h1 className="text-5xl font-medium">{score}</h1>
+
+            <div>
+                <img src={currentQuiz?.coverImageUrl} alt={currentQuiz?.name}/>
+                <h1 className="text-2xl font-medium">{currentQuiz?.name}</h1>
+            </div>
+            <div>
+                {
+                    currentQuiz?.questions.map((question) => (
+                        <div key={question.id}>
+                            <h1>{question.question}</h1>
+                            <div>
+                                {question.options.map((option) => (
+                                    <div 
+                                        className={`block text-lg bg-gray-50 w-80 p-4 m-4 rounded-md shadow 
+                                            ${option.isAnswer && "bg-green-500"} ${option.id === question.selectedOption && "bg-red-500"}`
+                                        }
+                                        key={option.id}
+                                        >{option.content}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
