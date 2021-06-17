446ed8a3c8b83b5f9a72af87fb02bdc0f98803ec
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-17 11:29:52 +0530

    Style: responsive home page

--- src/components/QuizGrid.tsx
+++ src/components/QuizGrid.tsx
@@ -4,12 +4,12 @@ import { useData } from "../context";
 export const QuizGrid = () => {
     const {state: {allQuiz}} = useData();
     return (
-        <div className="grid grid-flow-col gap-4">
+        <div className="md:grid md:grid-flow-col md:gap-4 flex flex-wrap flex-col">
             {
                 allQuiz?.map((quiz) => (
                     <Link 
                         key={quiz._id} to={`playzone/${quiz._id}`}>
-                        <div className="w-96 bg-white shadow-md rounded-xl overflow-hidden">
+                        <div className="w-full bg-white shadow-md rounded-xl overflow-hidden my-4 md:w-96 md:m-2">
                             <img src={quiz.coverImageUrl} alt={quiz.name}/>
                             <div className="p-4">
                                 <h1 className="text-lg">{quiz.name}</h1>
