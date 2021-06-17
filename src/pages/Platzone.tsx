cf438fb5a9f975a983475145c45dff88d741ab60
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-17 17:39:31 +0530

    hover effect added

--- src/pages/Platzone.tsx
+++ src/pages/Platzone.tsx
@@ -6,7 +6,7 @@ import { Option } from "../type/Quiz.type";
 export default function Playzone() {
     const { quizID } = useParams();
     const navigate = useNavigate();
-    const { state: {currentQuiz, questionNo, score}, dispatch } = useData();
+    const { state: {currentQuiz, questionNo}, dispatch } = useData();
     const currentQuestion = currentQuiz?.questions[questionNo];
 
     const scoreAndQuestionHandler = (option : Option) => {
@@ -42,17 +42,19 @@ export default function Playzone() {
     
     return (
         <div className="m-auto w-full max-w-2xl">
-            <h1 className="text-2xl font-medium">{score}</h1>
-            <h1 className="text-2xl">{currentQuiz?.name}</h1>
+            <h1 className="text-2xl text-center">{currentQuiz?.name}</h1>
+            <div className="font-medium text-gray-600"><span className="text-2xl text-black">
+                {questionNo + 1}</span>/{currentQuiz?.questions.length}
+            </div>
             <div>
-                <h1 className="text-xl font-medium p-2">{currentQuestion?.question}</h1>
+                <h1 className="text-xl font-medium p-2 mt-4">{currentQuestion?.question}</h1>
                 <div className="md:grid md:grid-flow-col md:grid-cols-2 md:grid-rows-2 md:gap-x-1 md:gap-y-0.5 flex flex-wrap flex-col">
                     {
                         currentQuestion?.options.map((option) => (
                             <button 
                                 key={option._id}
                                 onClick={() => optionHandler(option)}
-                                className="block text-lg bg-white w-80 p-4 mx-1 my-2 rounded-md shadow ">
+                                className="block text-lg bg-white w-80 p-4 mx-1 my-2 rounded-md shadow hover:bg-blue-500 hover:text-white">
                                 {option.content}
                             </button>
                         ))
