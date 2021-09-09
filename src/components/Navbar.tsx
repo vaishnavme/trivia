3d1352751e8892289dac050aaddd0ca89f8da435
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-09 18:17:51 +0530

    bug fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,4 @@
-import { Link } from 'react-router-dom';
-import { useData } from '../context';
-
 export const Navbar = () => {
-    const { dispatch } = useData();
-
     return (
         <div className="p-4 shadow">
             <h1 className="text-2xl font-medium text-pink-500 cursor-pointer">
