9169d9dc8a096d1de3aa7606af61fb42745f65c4
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-09-09 18:17:20 +0530

    Bug fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useData } from '../context';
 
 export const Navbar = () => {
+    const { dispatch } = useData();
+
     return (
         <div className="p-4 shadow">
-            <Link to="/">
-                <h1 className="text-2xl font-medium text-pink-500">Trivia</h1>
-            </Link>
+            <h1 className="text-2xl font-medium text-pink-500 cursor-pointer">
+                Trivia
+            </h1>
         </div>
     );
 };
